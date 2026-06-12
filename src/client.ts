a825e0a1f7196bccb258f903dec55102310a148c
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2026-06-11 18:22:24 -0700

    style(client): collapse intents array to single line

--- src/client.ts
+++ src/client.ts
@@ -2,9 +2,6 @@ import { Client, GatewayIntentBits } from "discord.js";
 
 export function createClient(): Client {
   return new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-    ],
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
   });
 }
